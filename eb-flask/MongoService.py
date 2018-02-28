c3cf58bffa068c0ec2588c9395555abeeced6779
Jose Lara <Jose@Joses-MacBook-Pro.local>
2018-02-27 20:29:34 -0500

    freaking pylint comments disabiling

--- eb-flask/MongoService.py
+++ eb-flask/MongoService.py
@@ -5,4 +5,6 @@ client = MongoClient("mongodb://duck_hacker:ssw690@34.228.66.29/videos")
 db = client.videos
 
 # print the number of documents in a collection
-print(db.get_collection('videos').find_one())
+data = db.get_collection('videos').find_one()
+
+print(data) #pylint: disable("print statement used")
