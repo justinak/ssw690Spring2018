8980f735aca00cd0f47a572861349c85a6c1a3eb
Jose Lara <Jose@Joses-MacBook-Pro.local>
2018-02-28 16:51:03 -0500

    adding chances

--- eb-flask/MongoService.py
+++ eb-flask/MongoService.py
@@ -1,10 +1,10 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://duck_hacker:ssw690@34.228.66.29/videos")
+client = MongoClient("mongodb://duck_hacker:ssw690@34.228.66.29/ssw690spring2018")
 
-db = client.videos
+db = client.ssw690spring2018
 
 # print the number of documents in a collection
-data = db.get_collection('videos').find_one()
+data = db.get_collection('ssw690spring2018').find_one()
 
 print(data) #pylint: disable("print statement used")
--- eb-flask/server.py
+++ eb-flask/server.py
@@ -1,15 +1,75 @@
+# from flask import Flask
+
+
+# # EB looks for an 'application' callable by default.
+# app = Flask(__name__)
+
+
+
+
+# @app.route("/videos")
+# def video():
+#     return 'Hello'
+
+
+# # run the app.
+# if __name__ == "__main__":
+#     app.debug = True
+#     app.run()
+
 from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
 
-
-# EB looks for an 'application' callable by default.
 app = Flask(__name__)
 
-@app.route("/videos")
-def video():
-    return 'Hello'
+app.config['MONGO_DBNAME'] = 'ssw690spring2018'
+app.config['MONGO_URI'] = 'mongodb://34.228.66.29:27017/ssw690spring2018'
+app.config['MONGO_USERNAME'] = 'duck_hacker'
+app.config['MONGO_PASSWORD'] = 'ssw690'
+app.config['MONGO_AUTH_MECHANISM'] = 'MONGODB-CR'
+
+mongo = PyMongo(app)
 
 
-# run the app.
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+@app.route('/')
+def get_all_videos():
+    '''Method returns all the videos on the database'''
+    return jsonify({'result': 'videos'})
+
+@app.route('/me', methods=['GET'])
+def get_all_stars():
+  star = mongo.db['MONGO_DBNAME']
+  output = []
+  d = star.find()
+  print(d)
+  if d:
+    for s in star.find():
+        output.append({'name' : s['name'], 'distance' : s['distance']})
+    return jsonify({'result' : output})
+  else:
+    return jsonify({'result': 'Nothing Found'})
+
+@app.route('/star/', methods=['GET'])
+def get_one_star(name):
+  data = mongo.db.ssw690spring2018
+  s = data.find_one({'name' : name})
+  if s:
+    output = {'name' : s['name'], 'distance' : s['distance']}
+  else:
+    output = "No such name"
+  return jsonify({'result' : output})
+
+# @app.route('/star', methods=['POST'])
+# def add_star():
+#   star = mongo.db.stars
+#   name = request.json['name']
+#   distance = request.json['distance']
+#   star_id = star.insert({'name': name, 'distance': distance})
+#   new_star = star.find_one({'_id': star_id })
+#   output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+#   return jsonify({'result' : output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
