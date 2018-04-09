74c47a72406209fb022faf994a9ec59612596fca
Jose Lara <Jose@host-studentw-137-48.dhcp.stevens-tech.edu>
2018-04-09 19:15:47 -0400

    adding routes for picture upload

--- eb-flask/settings.py
+++ eb-flask/settings.py
@@ -1,7 +1,7 @@
 
 # Mongo Configuration credentials
 MONGO_DBNAME = 'ssw690spring2018'
-MONGO_HOST  = '34.228.66.29'
+MONGO_HOST  = '34.230.77.217'
 MONGO_PORT  = '27017'
 MONGO_USERNAME = 'duck_hacker'
 MONGO_PASSWORD = 'ssw690'
